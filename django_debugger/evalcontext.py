ff8012d3900db95a6089103d8dfb84ff6007146d
Paczesiowa <paczesiowa@gmail.com>
2012-04-21 01:24:40 +0200

    get rid of pdb stuff from evalcontext (coverage library goes crazy after pdb instance is created)

--- django_debugger/evalcontext.py
+++ django_debugger/evalcontext.py
@@ -3,7 +3,6 @@
 from cStringIO import StringIO
 import traceback
 import threading
-import pdb
 import sys
 
 exec_lock = threading.Lock()
@@ -29,42 +28,15 @@ class EvalContext(object):
         exec_lock.acquire()
         save_stdout = sys.stdout
         try:
-            debugger = _OutputRedirectingPdb(save_stdout)
-            debugger.reset()
-            pdb.set_trace = debugger.set_trace
             sys.stdout = out
             try:
                 code = compile(s, '<web>', "single", 0, 1)
                 exec code in self.namespace, self.globs
-                debugger.set_continue()
             except KeyboardInterrupt:
                 raise
             except:
                 traceback.print_exc(file=out)
-                debugger.set_continue()
         finally:
             sys.stdout = save_stdout
             exec_lock.release()
         return out.getvalue()
-
-
-# From doctest
-class _OutputRedirectingPdb(pdb.Pdb):
-    """
-    A specialized version of the python debugger that redirects stdout
-    to a given stream when interacting with the user.  Stdout is *not*
-    redirected when traced code is executed.
-    """
-    def __init__(self, out):
-        self.__out = out
-        pdb.Pdb.__init__(self)
-
-    def trace_dispatch(self, *args):
-        # Redirect stdout to the given stream.
-        save_stdout = sys.stdout
-        sys.stdout = self.__out
-        # Call Pdb's trace dispatch method.
-        try:
-            return pdb.Pdb.trace_dispatch(self, *args)
-        finally:
-            sys.stdout = save_stdout
